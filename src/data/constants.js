656998f4d9007e716d8ad0605a3b4254a55ce542
daveesin <davisilvagoncalves0208@gmail.com>
2026-03-30 14:31:59 -0300

    Criação de arquivo que guarda os dados dos cards para facilitar manutenção e dar escalabilidade

---
+++ src/data/constants.js
@@ -0,0 +1,61 @@
+const kidsInfo = [
+    {
+        id: 1,
+        icon: "🗣️",
+        title: "Speaking Prático",
+        description: [
+            {text: "Aulas focadas em ", bold: false},
+            {text: "conversação", bold: true},
+            {text: " desde o primeiro dia, com materiais visuais e atividades lúdicas para estimular a comunicação natural e o interesse pelo idioma.", bold: false}
+        ]
+    },
+    {
+        id: 2,
+        icon: "📘",
+        title: "Material de Qualidade",
+        description: [
+            {text: "Material 100% em inglês apropriado para crianças, focado em ", bold: false},
+            {text: "interações e jogos", bold: true},
+            {text: " que estimulem a curiosidade e o aprendizado.", bold: false}
+        ]
+    },
+    {
+        id: 3,
+        icon: "🌍",
+        title: "Aplicação do Inglês para a Vida",
+        description: [
+            {text: "Seu filho aprende o idioma em experiências ", bold: false},
+            {text: "envolventes e dinâmicas", bold: true},
+            {text: ". Aqui é onde o inglês deixa de ser conteúdo e passa a ser ", bold: false},
+            {text: "ferramenta de expressão, descoberta e conexão com o mundo.", bold: true},
+        ]
+    }
+]
+
+
+const pilaresInfo = [
+    {
+        id: 1,
+        title: "Conectividade",
+        desc: "Criamos conexões reais entre alunos e o mundo. Levamos cultura, contexto e vida para dentro da sala, transformando o aprendizado em uma experiência que vai muito além do inglês."
+    },
+    {
+        id: 2,
+        title: "Inovação e Adaptabilidade",
+        desc: "O ensino evolui — e nós também. Buscamos constantemente novas formas de estruturar o aprendizado, adaptando cada estratégia às necessidades reais dos nossos alunos."
+    },
+    {
+        id: 3,
+        title: "Protagonismo do Aluno",
+        desc: "Aqui, o centro é você. Respeitamos seu ritmo, seus objetivos e sua história, colocando o aluno como protagonista do próprio desenvolvimento."
+    },
+    {
+        id: 4,
+        title: "Flexibilidade",
+        desc: "Sua rotina importa. Você tem liberdade para definir os horários das suas aulas semanalmente, aprendendo de forma leve e possível dentro da sua realidade."
+    }
+]
+
+
+
+export { kidsInfo, pilaresInfo }
